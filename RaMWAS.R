65139b6199e7b5c80b196133d30eeb2df9956228
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-09-13 10:07:06 -0400

    Create RaMWAS.R

--- RaMWAS.R
+++ RaMWAS.R
@@ -1,6 +1,6 @@
 library(ramwas)
 
-#run once
+#Run once
 library(BSgenome.Hsapiens.UCSC.hg19)
 cpgset = getCpGsetCG(BSgenome.Hsapiens.UCSC.hg19)
 saveRDS(file = "UCSC.hg19_cpgset.rds", object = cpgset)
@@ -29,11 +29,11 @@ ramwas1scanBams(param)
 ramwas2collectqc(param)
 #Open /qc/summary_bams/Summary_QC.txt to see summary QC.
 
-#figure
+#Figure
 summary <- read.csv("qc/summary_bams/Summary_QC.txt",sep = "\t")
 coldata <- read.csv("coldata.txt",sep = "\t")#metadata of your samples
 summary$Type <- coldata$GroupName
-png(file="total reads.png",width=500, height=500)
+png(file="Total reads.png",width=500, height=500)
 boxplot(as.numeric(gsub("," ,"", summary$Total.reads))/1000000~summary$Type, main = "Total sequenced reads", xlab = "",ylab = "Millions",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
 
@@ -41,14 +41,14 @@ png(file="High quality reads.png",width=500, height=500)
 boxplot(as.numeric(gsub("," ,"", summary$Reads.used.for.coverage))/1000000~summary$Type, main = "High quality reads", xlab = "",ylab = "Millions",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
 
-png(file="noise.png",width=500, height=500)
+png(file="Noise.png",width=500, height=500)
 boxplot(summary$Non.Cpg.CpG.coverage.ratio~summary$Type, main = "Noise", xlab = "",ylab = "non-CpG/CpG coverage ratio",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
 
-png(file="noCpG.png",width=500, height=500)
+png(file="NoCpG.png",width=500, height=500)
 boxplot(as.numeric(substr(summary$Non.CpG.reads....,1,4))~summary$Type, main = "Non-CpG reads percentage", xlab = "",ylab = "Percentage %",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
 
-png(file="peak.png",width=500, height=500)
+png(file="Peak.png",width=500, height=500)
 boxplot(summary$Peak.SQRT^2~summary$Type, main = "CpG density at peak", xlab = "",ylab = "CpG density",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
