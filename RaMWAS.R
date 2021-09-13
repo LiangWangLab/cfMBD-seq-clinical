f307bcdea36c3d87813272e48f5f25bca39e1f06
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-09-13 10:09:29 -0400

    Create RaMWAS.R

--- RaMWAS.R
+++ RaMWAS.R
@@ -4,6 +4,7 @@ library(ramwas)
 library(BSgenome.Hsapiens.UCSC.hg19)
 cpgset = getCpGsetCG(BSgenome.Hsapiens.UCSC.hg19)
 saveRDS(file = "UCSC.hg19_cpgset.rds", object = cpgset)
+
 #Put all bam files in a 'bams' folder
 files = list.files(path = "bams/", pattern = ".bam$") 
 files = gsub(".bam","",files)
@@ -16,14 +17,9 @@ param = ramwasParameters(
   scoretag = "MAPQ",
   minscore = 4,
   minfragmentsize = 100,
-  maxfragmentsize = 600,
-  minavgcpgcoverage = 0.3,
-  minnonzerosamples = 0.3,
-  filecovariates = "covariates.txt",
-  modelcovariates = "Batch",
-  modeloutcome = "Group",
-  modelPCs = 0
+  maxfragmentsize = 600
 )
+
 ##Quality control
 ramwas1scanBams(param)
 ramwas2collectqc(param)
@@ -32,23 +28,19 @@ ramwas2collectqc(param)
 #Figure
 summary <- read.csv("qc/summary_bams/Summary_QC.txt",sep = "\t")
 coldata <- read.csv("coldata.txt",sep = "\t")#metadata of your samples
-summary$Type <- coldata$GroupName
+summary$Type <- coldata$Type
 png(file="Total reads.png",width=500, height=500)
 boxplot(as.numeric(gsub("," ,"", summary$Total.reads))/1000000~summary$Type, main = "Total sequenced reads", xlab = "",ylab = "Millions",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
-
 png(file="High quality reads.png",width=500, height=500)
 boxplot(as.numeric(gsub("," ,"", summary$Reads.used.for.coverage))/1000000~summary$Type, main = "High quality reads", xlab = "",ylab = "Millions",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
-
 png(file="Noise.png",width=500, height=500)
 boxplot(summary$Non.Cpg.CpG.coverage.ratio~summary$Type, main = "Noise", xlab = "",ylab = "non-CpG/CpG coverage ratio",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
-
 png(file="NoCpG.png",width=500, height=500)
 boxplot(as.numeric(substr(summary$Non.CpG.reads....,1,4))~summary$Type, main = "Non-CpG reads percentage", xlab = "",ylab = "Percentage %",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
-
 png(file="Peak.png",width=500, height=500)
 boxplot(summary$Peak.SQRT^2~summary$Type, main = "CpG density at peak", xlab = "",ylab = "CpG density",col = "steelblue",border = "black", cex.lab = 1.5, cex.axis = 1.3, cex.main = 2)
 dev.off()
